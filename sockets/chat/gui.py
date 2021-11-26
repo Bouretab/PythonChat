5e146fa7127cae39edab01d2341840574b7a2818
Eliott Charbotel <65131713+Wanji-esv@users.noreply.github.com>
2021-11-26 19:23:34 +0100

    Tests

--- sockets/chat/gui.py
+++ sockets/chat/gui.py
@@ -16,7 +16,7 @@ window.geometry("480x300")
 window.minsize(300,300)
 window.iconbitmap("logo.ico")
 window.config(background = '#000000')
-window.attributes('-alpha',0.1)
+#window.attributes('-alpha',0.1) #Rend la fênetre transparante
 
 def receive():
     while True:
@@ -34,12 +34,16 @@ def receive():
 
 def write():
     while True:
-        message = f'{username} : {entry}'
+        message = f'{username} : {"test"}'
         client.send(message.encode('utf-8'))
+
+def test():
+    global entry
+    print(entry.get())
 
 input = Frame(window, bg='#000000', bd=1, relief=SUNKEN)
 entry = Entry(input, bg='#000000', fg='#ffffff', font='Arial' ).grid(row = 0, column = 0)
-bouton = Button(input, text="Envoyer", font=("arial"), bg='#000000', fg='#ffffff', command = write).grid(row = 0, column = 1 )
+bouton = Button(input, text="Envoyer", font=("arial"), bg='#000000', fg='#ffffff', command = test).grid(row = 0, column = 1 )
 input.grid(row = 1, column = 0)
 
 messages = Frame(window, bg='#000000', bd=1, relief=SUNKEN)
